6edf28b2d7ad32ceefe2e826b80e017740d2e965
lai95588 <47106943+lai95588@users.noreply.github.com>
2020-08-26 15:35:52 +0800

    Update and rename docker_install.sh to ubuntu_docker_install.sh

--- ubuntu_docker_install.sh
+++ ubuntu_docker_install.sh
@@ -11,16 +11,16 @@ sudo apt-get upgrade -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 echo "add the GPG key for the official Docker repository to your system"
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "Add the Docker repository to APT sources"
-sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian buster stable"
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu buster stable"
 
 echo "Redo apt update"
 sudo apt update
 sudo apt install -y avahi-daemon avahi-discover libnss-mdns libavahi-compat-libdnssd-dev g++ gcc jq socat
 
-echo "Make sure you are about to install from the Docker repo instead of the default debian repo"
+echo "Make sure you are about to install from the Docker repo instead of the default Ubuntu repo"
 apt-cache policy docker-ce
 
 sleep 5
